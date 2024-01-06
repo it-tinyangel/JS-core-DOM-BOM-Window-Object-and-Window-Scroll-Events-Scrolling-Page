414dcc02eadf041f07437e3749c39fb143d1c04e
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-06 04:57:20 +0200

    Second commit

--- js/script.js
+++ js/script.js
@@ -2,26 +2,58 @@ document.addEventListener('DOMContentLoaded', () => {
 	let scrollDownBox = document.getElementById('scrollDownBox');
 	let scrollUpBox = document.getElementById('scrollUpBox');
 
-	function increaseFontSize(scrollPosition) {
-		let newSize = 1 + (scrollPosition - 20) / (300 - 20) * (3 - 1);
+	function increaseFontSize(scrollPosition, initialFontSize, rangeStart, rangeEnd) {
+		let newSize = 1 + (scrollPosition - rangeStart) / (rangeEnd - rangeStart) * (3 - 1);
+		newSize = Math.min(Math.max(newSize, 1), 3);
+		return newSize * initialFontSize;
+	}
+
+	function decreaseFontSize(scrollPosition, rangeStart, rangeEnd) {
+		let newSize = 3 - (scrollPosition - rangeStart) / (rangeEnd - rangeStart) * (3 - 1);
 		newSize = Math.min(Math.max(newSize, 1), 3);
 		return newSize;
 	}
 
-	function decreaseFontSize() {
-		return 3;
-	}
-
 	function handleScroll() {
 		let scrollPosition = window.scrollY;
+		let windowWidth = window.innerWidth;
 
-		if (scrollPosition > 20 && scrollPosition <= 300) {
-			let newSize = increaseFontSize(scrollPosition);
-			scrollDownBox.style.fontSize = `${newSize}em`;
+		console.log('scrollPosition:', scrollPosition);
+		console.log('windowWidth:', windowWidth);
+
+		if (scrollPosition > 0 && scrollPosition <= 300) {
+			let newSizeFirstSection = increaseFontSize(scrollPosition, 1, 0, 300);
+			scrollDownBox.style.fontSize = `${newSizeFirstSection}em`;
+
 			console.log('scrollDownBox fontSize:', scrollDownBox.style.fontSize);
 		}
 
-		scrollUpBox.style.fontSize = `${decreaseFontSize()}em`;
+		if (windowWidth < 679) {
+			if (scrollPosition > 300 && scrollPosition <= 600) {
+				let newSizeThirdSection = decreaseFontSize(scrollPosition, 300, 600);
+				scrollUpBox.style.fontSize = `${newSizeThirdSection}em`;
+
+				console.log('scrollUpBox fontSize:', scrollUpBox.style.fontSize);
+			}
+		}
+
+		if (windowWidth >= 679 && windowWidth < 1024) {
+			if (scrollPosition > 500 && scrollPosition <= 800) {
+				let newSizeThirdSection = decreaseFontSize(scrollPosition, 500, 800);
+				scrollUpBox.style.fontSize = `${newSizeThirdSection}em`;
+
+				console.log('scrollUpBox fontSize:', scrollUpBox.style.fontSize);
+			}
+		}
+
+		if (windowWidth >= 1024) {
+			if (scrollPosition > 800 && scrollPosition <= 1100) {
+				let newSizeThirdSection = decreaseFontSize(scrollPosition, 700, 900);
+				scrollUpBox.style.fontSize = `${newSizeThirdSection}em`;
+
+				console.log('scrollUpBox fontSize:', scrollUpBox.style.fontSize);
+			}
+		}
 	}
 
 	handleScroll();
