6e8d11c1ee038319756ab73db6b23b5d434d47d9
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-08 05:00:26 +0200

    Updated js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,21 @@
-let scrollDownBox = document.getElementById('scrollDownBox');
-let scrollUpBox = document.getElementById('scrollUpBox');
+document.addEventListener('DOMContentLoaded', () => {
+	let scrollMove = document.querySelector('.move-to');
 
-let scrollSlideBox = document.getElementById('scrollSlideBox');
+	function handleScroll(scrollBox, offsetValue) {
+		window.onscroll = () => {
+			let top = window.scrollY;
+			let offset = scrollBox.offsetTop + offsetValue;
+			let height = scrollBox.offsetHeight;
 
-// let scrollDownText = document.getElementById('scrollDownText');
-// let scrollUpText = document.getElementById('scrollUpText');
-
-// let scrollSlideText = document.getElementById('scrollSlideText');
-// let scrollSlideLine = document.getElementById('scrollSlideLine');
-// let scrollSlideImage = document.getElementById('scrollSlideImage');
-
-// console.log('window.scrollY:', window.scrollY);
-// console.log('window.innerWidth:', window.innerWidth);
-// console.log('scrollDownBox.offsetTop:', scrollDownBox.offsetTop);
-// console.log('scrollDownBox.offsetHeight:', scrollDownBox.offsetHeight);
-
-function handleScroll(scrollBox,offsetValue) {
-	window.onscroll = () => {
-		let top = window.scrollY;
-		let offset = scrollBox.offsetTop + offsetValue;
-		let height = scrollBox.offsetHeight;
-
-		if (top >= offset && top < offset + height) {
-			scrollBox.classList.add('show-animate');
-			console.log(scrollBox);
-		}
-		else {
-			scrollBox.classList.remove('show-animate');
+			if (top >= offset && top < offset + height) {
+				scrollBox.classList.add('show-animate');
+				console.log(scrollBox);
+			}
+			else {
+				scrollBox.classList.remove('show-animate');
+			}
 		}
 	}
-}
 
-handleScroll(scrollDownBox, +300);
-handleScroll(scrollUpBox, +300);
-handleScroll(scrollSlideBox, -150);
+	handleScroll(scrollMove, -200);
+});
