f9d78f7d4ff9b3a464df5ac359bac55fded3a0fc
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-09 20:50:42 +0200

    Updated js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let scrollScaleDown = document.querySelector('.scale-down'),
-		scrollMove = document.querySelector('.move-from'),
-		scrollScaleUp = document.querySelector('.scale-up');
+	let scrollScaleDown = document.querySelector('.scale-down');
+	let scrollMove = document.querySelector('.move-from');
+	let scrollScaleUp = document.querySelector('.scale-up');
+
+	let scaleDownText = document.querySelector('.scale-down__text');
+	let scaleUpText = document.querySelector('.scale-up__text');
 
 	// Function to handle scroll animation for a given scrollBox
 	function handleScroll(scrollBox) {
 		let windowHeight = window.innerHeight;
 		let boxTop = scrollBox.offsetTop;
 		let boxHeight = scrollBox.offsetHeight;
-		let boxMiddle = boxTop + boxHeight / 2;
 
-		// Calculate offset from window middle to scrollBox middle
-		let windowMiddle = windowHeight / 2;
-		let offset = window.scrollY + windowMiddle - boxMiddle;
+		// Calculation of the middle of the window when scrolling window
+		let offset = window.scrollY + windowHeight / 2 - (boxTop + boxHeight / 2);
 
 		if (Math.abs(offset) < windowHeight / 4) {
 			scrollBox.classList.add('show-animate');
@@ -27,7 +28,23 @@ document.addEventListener('DOMContentLoaded', () => {
 		handleScroll(scrollScaleUp);
 	}
 
+	function scrollSmoothToTop() {
+		window.scroll({
+			top: 0,
+			behavior: 'smooth'
+		});
+	}
+
+	function scrollToCenter() {
+		scrollMove.scrollIntoView({
+			behavior: 'smooth',
+			block: 'center'
+		});
+	}
+
 	window.addEventListener('scroll', scrollHandler);
+	scaleUpText.addEventListener("click", scrollSmoothToTop);
+	scaleDownText.addEventListener('click', scrollToCenter);
 
 	scrollHandler();
 });
